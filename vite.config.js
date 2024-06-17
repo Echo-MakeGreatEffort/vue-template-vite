4ec541a08a5efa716a2cc15a559a06fd1edd2bb5
liying <9971651+caigou111@user.noreply.gitee.com>
2024-06-17 19:34:53 +0800

    feat: 修改配置

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { fileURLToPath, URL } from 'node:url';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  alias: {
-    '/@': path.resolve(__dirname, 'src')
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   plugins: [
     vue(),
